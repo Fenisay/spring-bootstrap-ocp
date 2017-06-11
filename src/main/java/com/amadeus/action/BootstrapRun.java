40a5a4244d21b4348d6dc47bdecd1f0f1727da3f
Yassine Ferkouch <yassine.ferkouche@amadeus.com>
2017-06-11 18:08:48 +0200

    fabric8

--- src/main/java/com/amadeus/action/BootstrapRun.java
+++ src/main/java/com/amadeus/action/BootstrapRun.java
@@ -3,9 +3,7 @@ package com.amadeus.action;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Profile;
- 
+
 
 @SpringBootApplication
 public class BootstrapRun {
